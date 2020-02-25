799ab9aa30196c8f020eb87f4f9f4e443f3286e9
monkeychen528 <53263222+monkeychen528@users.noreply.github.com>
2020-02-25 19:51:52 +0800

    socket簡單聊天室分流

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,61 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import websocket from 'socket.io-client';
 import Navbar from './Navbar';
+import '../asset/comment.css';
 
 const Comment = () => {
   const [ws, setWs] = useState(null);
+  const [room, setRoom] = useState(null);
+  const msg = document.querySelector('#msg');
 
   const connectWebSocket = () => {
+    if (room) setWs(websocket('http://localhost:3000', { path: room }));
     setWs(websocket('http://localhost:3000'));
   };
   // 監聽送回的訊息
   const initWebSocket = () => {
-    ws.on('getMessage', (msg) => {
-      console.log(msg);
+    ws.on('addroom', (getMsg) => {
+      console.log(getMsg);
       const showText = document.querySelector('#textBlock');
       const block = document.createElement('p');
-      block.innerHTML = msg;
+      block.innerHTML = getMsg;
+      showText.append(block);
+    });
+
+    ws.on('getMessage', (getMsg) => {
+      console.log(getMsg);
+      const showText = document.querySelector('#textBlock');
+      const block = document.createElement('p');
+      block.innerHTML = getMsg;
       showText.append(block);
     });
   };
 
+
+  // 送出訊息到websocket
+  const sendMessage = () => {
+    // console.log(room);
+    ws.emit(room ? 'changeroom' : 'getMessage',
+      room ? { nowRoom: room, msg: msg.value } : msg.value);
+  };
+  // 更換房間
+  const changeRoom = (e) => {
+    const roomName = e.target.innerHTML;
+    ws.emit('changeroom', { nowRoom: roomName });
+    setRoom(roomName);
+    console.log(roomName);
+  };
+
+
   useEffect(() => {
     if (ws) {
       console.log('success');
-      console.log(ws.disconnected);
       initWebSocket();
     }
     if (ws === null) connectWebSocket();
-    // return (() => {
-    //   //   window.onbeforeunload = () => {
-    //   //     'nbeforeunload is work';
-    //   //   };
-    //   ws.socket.close();
-    // });
   }, [ws]);
-  // 送出訊息到websocket
-  const sendMessage = () => {
-    const msg = document.querySelector('#msg');
-    ws.emit('getMessage', msg.value);
-  };
+
 
   console.log(ws);
   return (
     <>
       <Navbar />
-      <div className="roomBody d-flex">
+      <div className="roomBody">
         <aside>
           <div className="roomWrap">
             <ul>
-              <li className="room">房間一</li>
-              <li className="room">房間二</li>
-              <li className="room">房間三</li>
+              <li className="room" onClick={changeRoom} role="presentation">房間一</li>
+              <li className="room" onClick={changeRoom} role="presentation">房間二</li>
+              <li className="room" onClick={changeRoom} role="presentation">房間三</li>
             </ul>
           </div>
         </aside>
-        <div className="textWrap">
-          <div id="textBlock">
-            文字訊息
+        <div>
+          <div className="textWrap">
+            <div id="textBlock">
+              文字訊息
+            </div>
           </div>
           <input type="text" placeholder="輸入文字" id="msg" />
           <input type="button" onClick={sendMessage} value="送出" />
